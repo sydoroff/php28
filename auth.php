fa5d4db89b683c1399c7a1c3880808726751c621
sydoroff <is.sydoroff@gmail.com>
2018-11-29 01:05:15 +0200

    Интеграция пользователя

---
+++ auth.php
@@ -0,0 +1,1 @@
+<?php/** * Created by PhpStorm. * User: юзер * Date: 28.11.2018 * Time: 0:29 */session_start();require_once ('User.php');$user = new UserRun();$user->run($_POST['email'],$_POST['passwd'],$_GET['action']);
